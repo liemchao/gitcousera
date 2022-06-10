85538dfecfad6afd17b928bc5957d650dd6b41be
liemro9x@gmail.com <liemro9x@gmail.com>
2022-06-10 20:09:02 +0700

    new project

---
+++ index.js
@@ -0,0 +1,47 @@
+
+// let integer = 5;
+// let bolen = true;
+// let name = "liem";
+// let _x= 10;
+// let y ;
+// const PI = 3.14;
+// let z = 3.14;
+// let car=[
+//! day la doan code thieu nhi
+//     'Mesades Ben',
+//     'Gai Mot con',
+//     'EM gai loli',
+//     'Em gai xinh tuoi'
+// ]
+// console.log();
+
+// if(z==_x){
+//     console.log("Anh yeu em nhieu lam");
+
+// }
+//* thuat toan de xac dinh if cho vong lap
+//? the let la khai bao sua dung trong block code
+//? the var la kai bao sua dung chung
+//? the const la khai bao hang so
+//* nhung toan tu la <= >= === == a++ a--  -a  +a
+//* mang la arr[1,2,3,4]
+let mark = 4.9;
+
+if (mark >= 0 && mark < 5) {
+
+    console.log("Hoc sinh kem");
+
+} else if (mark >= 5 && mark < 6.5) {
+    console.log("Hoc sinh tb");
+
+} else if (mark >= 6.5 && mark < 8) {
+
+    console.log("hoc sinh kha");
+
+} else if (mark >= 8 && mark <= 9) {
+
+    console.log("hoc sinh gioi");
+
+} else
+
+    console.log("hoc sinh xuat sac");
